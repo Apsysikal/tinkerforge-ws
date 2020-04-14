ada0eb82b5ff402c89e7330bed69e09df2e3228a
Caserage <schnieppbenedikt@gmail.com>
2020-04-14 14:01:42 +0200

    Added typescript.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+///<reference path="../node_modules/tinkerforge/Tinkerforge.js" />
+///<reference path="../node_modules/tinkerforge/lib/IPConnection.js" />
+
+import * as tinkerforge from "tinkerforge";
+
